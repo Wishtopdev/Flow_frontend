710734d65b0fe3c1976cd83f570859f8c71e54ea
Wishtopdev <ultraj0330@gmail.com>
2020-05-07 12:58:03 -0700

    change dist

---
+++ dist/precache-manifest.4be7b182d81db6df7fc3b3bb90aa217e.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f72b3fed184c7227f952041c5ec3535",
+    "url": "/metronic/preview/react/demo1/index.html"
+  },
+  {
+    "revision": "7c777325bb936f704426",
+    "url": "/metronic/preview/react/demo1/static/css/3.22f59f61.chunk.css"
+  },
+  {
+    "revision": "bd12876f741bb2cf483d",
+    "url": "/metronic/preview/react/demo1/static/css/main.c414f51c.chunk.css"
+  },
+  {
+    "revision": "f6e673ccb608b0363490",
+    "url": "/metronic/preview/react/demo1/static/js/0.288255cd.chunk.js"
+  },
+  {
+    "revision": "7c777325bb936f704426",
+    "url": "/metronic/preview/react/demo1/static/js/3.0d1b2cd1.chunk.js"
+  },
+  {
+    "revision": "447ee4d826d1bafb05d3",
+    "url": "/metronic/preview/react/demo1/static/js/4.ef6a20c9.chunk.js"
+  },
+  {
+    "revision": "55727e6f7daa786485ee",
+    "url": "/metronic/preview/react/demo1/static/js/5.a93565cf.chunk.js"
+  },
+  {
+    "revision": "c6422cb01446bd4b0663",
+    "url": "/metronic/preview/react/demo1/static/js/6.ba6999b0.chunk.js"
+  },
+  {
+    "revision": "49b463307fe5f9cf1e59",
+    "url": "/metronic/preview/react/demo1/static/js/7.169496f1.chunk.js"
+  },
+  {
+    "revision": "d3fec3f35602a2729a53",
+    "url": "/metronic/preview/react/demo1/static/js/8.e417b7dc.chunk.js"
+  },
+  {
+    "revision": "c50bc704d1468ebb4ab7",
+    "url": "/metronic/preview/react/demo1/static/js/9.9bb73ad6.chunk.js"
+  },
+  {
+    "revision": "bd12876f741bb2cf483d",
+    "url": "/metronic/preview/react/demo1/static/js/main.701efafe.chunk.js"
+  },
+  {
+    "revision": "6441dec3929178a4eab0",
+    "url": "/metronic/preview/react/demo1/static/js/runtime-main.d79fddbf.js"
+  },
+  {
+    "revision": "35d544eaaa4cf3c6355866280d53ba73",
+    "url": "/metronic/preview/react/demo1/static/media/Flaticon.35d544ea.eot"
+  },
+  {
+    "revision": "3e4331ee31764c999add7e0b048c4ba3",
+    "url": "/metronic/preview/react/demo1/static/media/Flaticon.3e4331ee.ttf"
+  },
+  {
+    "revision": "500d63cca8c534718b1d202b2076d218",
+    "url": "/metronic/preview/react/demo1/static/media/Flaticon.500d63cc.svg"
+  },
+  {
+    "revision": "5be3e43c13c3eb021d15e6682d098d4c",
+    "url": "/metronic/preview/react/demo1/static/media/Flaticon.5be3e43c.woff"
+  },
+  {
+    "revision": "29586ff0f963f4d1fdfc182822b8b27a",
+    "url": "/metronic/preview/react/demo1/static/media/Flaticon2.29586ff0.eot"
+  },
+  {
+    "revision": "4248a89254d2c6c396d85e378184c54f",
+    "url": "/metronic/preview/react/demo1/static/media/Flaticon2.4248a892.svg"
+  },
+  {
+    "revision": "b242ac810bd8cccaa03abc2128b7c3c3",
+    "url": "/metronic/preview/react/demo1/static/media/Flaticon2.b242ac81.woff"
+  },
+  {
+    "revision": "eafcbac04cdb0a39fe38a36ebd786290",
+    "url": "/metronic/preview/react/demo1/static/media/Flaticon2.eafcbac0.ttf"
+  },
+  {
+    "revision": "670df2d1373e4135c464502f80a1784c",
+    "url": "/metronic/preview/react/demo1/static/media/Ki.670df2d1.ttf"
+  },
+  {
+    "revision": "770f181b58cb3f1ec7a902b7842de863",
+    "url": "/metronic/preview/react/demo1/static/media/Ki.770f181b.eot"
+  },
+  {
+    "revision": "f030d6f8c0a50ba57e6e82d3977ee120",
+    "url": "/metronic/preview/react/demo1/static/media/Ki.f030d6f8.svg"
+  },
+  {
+    "revision": "f2362a7b881b57280ab585a85ec61a8f",
+    "url": "/metronic/preview/react/demo1/static/media/Ki.f2362a7b.woff"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/metronic/preview/react/demo1/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/metronic/preview/react/demo1/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/metronic/preview/react/demo1/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/metronic/preview/react/demo1/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/metronic/preview/react/demo1/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/metronic/preview/react/demo1/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/metronic/preview/react/demo1/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/metronic/preview/react/demo1/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/metronic/preview/react/demo1/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/metronic/preview/react/demo1/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/metronic/preview/react/demo1/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/metronic/preview/react/demo1/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/metronic/preview/react/demo1/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/metronic/preview/react/demo1/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/metronic/preview/react/demo1/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "60e5857089e98edd838074c264d6c951",
+    "url": "/metronic/preview/react/demo1/static/media/socicon.60e58570.eot"
+  },
+  {
+    "revision": "944f06f5f65ef84a3a36e6c1c2d4b7ad",
+    "url": "/metronic/preview/react/demo1/static/media/socicon.944f06f5.woff"
+  },
+  {
+    "revision": "9a64ef938f9e55a70a4defc6ac9790bf",
+    "url": "/metronic/preview/react/demo1/static/media/socicon.9a64ef93.ttf"
+  },
+  {
+    "revision": "a35b65744f557fab5424e99bb6d4e980",
+    "url": "/metronic/preview/react/demo1/static/media/socicon.a35b6574.svg"
+  }
+]);
